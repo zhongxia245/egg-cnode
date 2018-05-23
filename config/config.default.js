aaf4b07324cf361b52d1196e1b2963e761655e76
zhongxia245 <zhongxia245@sina.com>
2018-05-23 21:12:20 +0800

    update view

--- config/config.default.js
+++ config/config.default.js
@@ -1,45 +1,46 @@
-'use strict';
-const path = require('path');
+'use strict'
+const path = require('path')
 
 module.exports = appInfo => {
-  const config = {};
+  const config = {}
 
-  config.name = 'Zhongxia Blog';
+  config.name = 'Zhongxia'
 
-  config.description = 'Summarize, record and grow';
+  config.description = 'Zhongxia Blog | Summarize, record and grow'
 
-  config.site_logo = 'http://p8x4ztqeg.bkt.clouddn.com/icon/icon.svg';
+  // logo , 默认为 config.name
+  config.site_logo = ''
 
-  config.site_icon = 'http://p8x4ztqeg.bkt.clouddn.com/icon/icon.svg';
+  config.site_icon = 'http://p8x4ztqeg.bkt.clouddn.com/icon/icon.svg'
 
   // debug 为 true 时，用于本地调试
-  config.debug = true;
+  config.debug = true
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = appInfo.name + '_1519887194138_3450';
+  config.keys = appInfo.name + '_1519887194138_3450'
 
-  config.host = 'http://blog.izhongxia.com';
+  config.host = 'http://blog.izhongxia.com'
 
-  config.session_secret = 'egg_blog_zhongxia'; // 务必修改
+  config.session_secret = 'egg_blog_zhongxia' // 务必修改
 
   // add your config here
-  config.middleware = [ 'locals', 'authUser', 'blockUser', 'errorPage' ];
+  config.middleware = ['locals', 'authUser', 'blockUser', 'errorPage']
 
   config.authUser = {
     enable: true,
-    match: '/',
-  };
+    match: '/'
+  }
 
   // 是否允许直接注册（否则只能走 github 的方式）
-  config.allow_sign_up = true;
+  config.allow_sign_up = true
 
   // cdn host，如 http://cnodejs.qiniudn.com
-  config.site_static_host = process.env.EGG_SITE_STATIC_HOST || ''; // 静态文件存储域名
+  config.site_static_host = process.env.EGG_SITE_STATIC_HOST || '' // 静态文件存储域名
 
-  config.mini_assets = process.env.EGG_MINI_ASSETS || false;
+  config.mini_assets = process.env.EGG_MINI_ASSETS || false
 
   // 版块
-  config.tabs = [[ 'share', '分享' ], [ 'ask', '问答' ]];
+  config.tabs = [['share', '分享'], ['ask', '问答']]
 
   // RSS配置
   config.rss = {
@@ -48,8 +49,8 @@ module.exports = appInfo => {
     language: 'zh-cn',
     description: 'Zhongxia BLog: 记录、总结、成长',
     // 最多获取的RSS Item数量
-    max_rss_items: 50,
-  };
+    max_rss_items: 50
+  }
 
   // 下面两个配置都是文件上传的配置
 
@@ -61,26 +62,26 @@ module.exports = appInfo => {
     origin: 'http://static.izhongxia.com',
     // 如果vps在国外，请使用 http://up.qiniug.com/ ，这是七牛的国际节点
     // 如果在国内，此项请留空
-    uploadURL: 'http://xxxxxxxx',
-  };
+    uploadURL: 'http://xxxxxxxx'
+  }
 
   // 文件上传配置
   // 注：如果填写 qn_access，则会上传到 7牛，以下配置无效
   config.upload = {
     path: path.join(__dirname, '../app/public/upload/'),
-    url: '/public/upload/',
-  };
+    url: '/public/upload/'
+  }
 
   config.view = {
     defaultViewEngine: 'ejs',
     mapping: {
-      '.html': 'ejs',
-    },
-  };
+      '.html': 'ejs'
+    }
+  }
 
   config.ejs = {
-    layout: 'layout.html',
-  };
+    layout: 'layout.html'
+  }
 
   config.avatars_allow_hostname = [
     'avatars0.githubusercontent.com',
@@ -89,17 +90,17 @@ module.exports = appInfo => {
     'avatars.githubusercontent.com',
     'www.gravatar.com',
     'gravatar.com',
-    'www.google-analytics.com',
-  ];
+    'www.google-analytics.com'
+  ]
 
-  config.auth_cookie_name = 'node_club';
+  config.auth_cookie_name = 'node_club'
   config.admins = {
-    ADMIN_USER: true,
-  };
+    ADMIN_USER: true
+  }
 
   config.siteFile = {
-    '/favicon.ico': '/public/images/cnode_icon_32.png',
-  };
+    '/favicon.ico': '/public/images/cnode_icon_32.png'
+  }
 
   // database
   config.redis = {
@@ -107,9 +108,9 @@ module.exports = appInfo => {
       host: process.env.EGG_REDIS_HOST || '127.0.0.1',
       port: process.env.EGG_REDIS_PORT || 6379,
       password: process.env.EGG_REDIS_PASSWORD || '',
-      db: process.env.EGG_REDIS_DB || '0',
-    },
-  };
+      db: process.env.EGG_REDIS_DB || '0'
+    }
+  }
 
   /**
    * @see http://mongodb.github.io/node-mongodb-native/2.2/api/Db.html#createCollection
@@ -117,20 +118,20 @@ module.exports = appInfo => {
   config.mongoose = {
     url: process.env.EGG_MONGODB_URL || 'mongodb://127.0.0.1:27017/egg_cnode',
     options: {
-      server: { poolSize: 20 },
-    },
-  };
+      server: { poolSize: 20 }
+    }
+  }
 
   // passport
   config.passportGithub = {
     key: process.env.EGG_PASSPORT_GITHUB_CLIENT_ID || 'test',
-    secret: process.env.EGG_PASSPORT_GITHUB_CLIENT_SECRET || 'test',
-  };
+    secret: process.env.EGG_PASSPORT_GITHUB_CLIENT_SECRET || 'test'
+  }
 
   config.passportLocal = {
     usernameField: 'name',
-    passwordField: 'pass',
-  };
+    passwordField: 'pass'
+  }
 
   // 邮箱配置
   config.mail_opts = {
@@ -138,36 +139,36 @@ module.exports = appInfo => {
     port: 25,
     auth: {
       user: 'club@126.com',
-      pass: 'club',
+      pass: 'club'
     },
-    ignoreTLS: true,
-  };
+    ignoreTLS: true
+  }
 
   config.alinode = {
     // 从 `Node.js 性能平台` 获取对应的接入参数
     appid: process.env.EGG_ALINODE_APPID || '',
-    secret: process.env.EGG_ALINODE_SECRET || '',
-  };
+    secret: process.env.EGG_ALINODE_SECRET || ''
+  }
 
   config.topic = {
-    perDayPerUserLimitCount: 100,
-  };
+    perDayPerUserLimitCount: 100
+  }
 
-  config.list_topic_count = 20;
+  config.list_topic_count = 20
 
   // 每个 IP 每天可创建用户数
-  config.create_user_per_ip = 1000;
+  config.create_user_per_ip = 1000
 
-  config.search = 'local'; // 'google', 'baidu', 'local'
+  config.search = 'local' // 'google', 'baidu', 'local'
 
   config.security = {
     csrf: {
-      ignore: '/api/*/*',
-    },
-  };
+      ignore: '/api/*/*'
+    }
+  }
 
-  config.default_page = 1;
-  config.default_limit = 20;
+  config.default_page = 1
+  config.default_limit = 20
 
-  return config;
-};
+  return config
+}
